05529b7338bc891ab0183d6cab459a1e01d05200
Jim Zhu <jimzhu41@gmail.com>
2017-06-13 14:04:33 -0400

    First commit

---
+++ src/main/java/com/cornerstones/CornerstonesApplication.java
@@ -0,0 +1,12 @@
+package com.cornerstones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CornerstonesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CornerstonesApplication.class, args);
+	}
+}
